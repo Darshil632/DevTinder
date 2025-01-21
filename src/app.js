3ff08c1a569d5b7efef686b63572b94c0426be9b
Darshil632 <darshilvijay632@gmail.com>
2025-01-21 20:02:59 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
+app.get(/a/, (req, res) => {
+  res.send({ firstnmae: "Darshil", lastname: "Vijay" });
 });
-app.use("/home", (req, res) => {
-  res.send("Hello from the home");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("Deeleted Successfully!");
+// });
+// app.use("/home", (req, res) => {
+//   res.send("Hello from the home");
+// });
+// app.use("/", (req, res) => {
+//   res.send("Hello from the server");
+// });
 app.listen(3000, () => {
   console.log("Server is starting........");
 });
