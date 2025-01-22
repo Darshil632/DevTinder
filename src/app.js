097b16ea109950d2b067d834f4d3187529196d40
Darshil632 <darshilvijay632@gmail.com>
2025-01-22 14:23:53 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,29 +1,44 @@
 const express = require("express");
+const { adminAuth, userauth } = require("./middlewares/auth");
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Route Handler 1!!");
-    next();
-    // res.send("1st  response");
-  },
-  (req, res, next) => {
-    console.log("Route Handler 2!!");
-    // res.send("2nd  response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route Handler 3!!");
-    // res.send("3rd  response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route Handler 4!!");
-    res.send("4th  response");
-    next();
-  }
-);
+app.use("/admin", adminAuth);
+app.get("/admin/getAllUser", (req, res) => {
+  res.send("All data Sent!");
+});
+app.get("/admin/deleteuser", (req, res) => {
+  res.send("All data deleted!");
+});
+app.get("/user/login", (req, res) => {
+  res.send("User logged in successfully");
+});
+app.use("/user", userauth, (req, res) => {
+  res.send("User data sent");
+});
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Route Handler 1!!");
+//     next();
+//     // res.send("1st  response");
+//   },
+//   (req, res, next) => {
+//     console.log("Route Handler 2!!");
+//     // res.send("2nd  response");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Route Handler 3!!");
+//     // res.send("3rd  response");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Route Handler 4!!");
+//     res.send("4th  response");
+//     next();
+//   }
+// );
 
 // app.get(/a/, (req, res) => {
 //   res.send({ firstnmae: "Darshil", lastname: "Vijay" });
