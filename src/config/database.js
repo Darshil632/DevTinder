8ce985f330f0ceb2bc1afbd0149dfb329fbb910b
Darshil Vijayvargiya <94682792+Darshil632@users.noreply.github.com>
2025-01-22 18:00:17 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://darshilvijay632:Darshil2025@cluster1.knu2n.mongodb.net/devTinder"
+    "MongoDB_URL"
   );
 };
 module.exports = connectDB;
